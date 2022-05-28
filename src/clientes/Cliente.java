7ab2c27ce2f12520e3768869ee5b3c6d9d131200
Nícalo Ribeiro <nicalo63@gmail.com>
2022-05-28 01:47:39 -0300

    Criacao da classe base para o cliente

--- src/clientes/Cliente.java
+++ src/clientes/Cliente.java
@@ -1,5 +1,21 @@
 package clientes;
 
 public class Cliente {
-    
+
+    private String nome;
+    private String sobrenome;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
 }
