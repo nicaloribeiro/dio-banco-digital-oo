a9a1702ad785a9bd26e285a30d457f243d1027d6
Nícalo Ribeiro <nicalo63@gmail.com>
2022-05-28 01:48:02 -0300

    Criacao da interface base para a classe conta

---
+++ src/contas/interfaces/IConta.java
@@ -0,0 +1,15 @@
+package contas.interfaces;
+
+import contas.Conta;
+
+public interface IConta {
+
+    void sacar(double valor);
+	
+	void depositar(double valor);
+	
+	void transferir(double valor, Conta contaDestino);
+	
+	void imprimirExtrato();
+    
+}
